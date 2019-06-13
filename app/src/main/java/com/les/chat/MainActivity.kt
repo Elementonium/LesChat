c5bf498a65224d910e91e013f6795d94bab57531
naveenbalachandran <bnaveen360@gmail.com>
2019-06-13 10:14:18 +0530

    -Login/Register Buttons Now Works -UI changes for Login/Register Forms -Integrated to Firebase Database

--- app/src/main/java/com/les/chat/MainActivity.kt
+++ app/src/main/java/com/les/chat/MainActivity.kt
@@ -3,6 +3,8 @@ package com.les.chat
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-        val email = email_edittext_register.text.toString()
-        val password = password_edittext_login.text.toString()
+            performRegister()
+
         }
         already_have_account_text_view.setOnClickListener{
             val intent = Intent(this,LoginActivity::class.java)
@@ -21,4 +23,21 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please Enter Email/Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener  //if user creation fails go back
+
+            }.addOnFailureListener {
+                Toast.makeText(this,"Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
